3f9580c0cf6aa413b26444d5e111c778ff722114
Marcos Bento <marcos.x.marcos@hotmail.com>
2024-04-03 11:52:47 -0300

    Finalizando aula 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { styled } from "styled-components"
 import EstilosGlobais from "./Componentes/EstilosGlobais/index"
+import Cabecalho from "./Componentes/Cabecalho"
+import BarraLateral from "./Componentes/BarraLateral"
+import Banner from "./Componentes/TituloEstilizado"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244f 48%, #154580 96.76%);
@@ -12,7 +15,8 @@ function App() {
   return (
     <FundoGradiente>
       <EstilosGlobais/>
-      Olá mundo 
+      <Cabecalho/>
+      <BarraLateral/>
     </FundoGradiente>
   )
 }
